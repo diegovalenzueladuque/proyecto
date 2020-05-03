a52852f2d1e95eb2f589bfc7cfb91ad622c8a6d2
Diego Valenzuela Duque <diegovalenz@uchile.cl>
2020-05-02 22:48:44 -0400

    first commit

---
+++ instructivos.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>UNIDAD STC. INSTRUCTIVOS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lightbox.css">
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="jumbotron my-auto">
+			<div class="row">
+				<div class="col-6 col-sm-6 col-md-2 col-lg-1 col-xl-1 my-auto"><img src="img/logo_u.png" alt="" style="width: 60px; border-top: 0px" class="my-auto"></div>
+				<div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 my-auto"><h5 style="color: #fff" class="my-auto">UNIVERSIDAD DE CHILE</h4>
+					<h5 style="color: #fff" class="my-auto">FACULTAD DE ARTES</h5></div>
+				<div class="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-7 my-auto"><h3 style="color: #fff">UNIDAD STC</h3></div>
+			</div>
+			
+		</div>
+	</div>
+		<div class="container-fluid">
+				
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="index.php">INICIO</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse"
+				data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+				<a class="nav-link" href="equipo.php">EQUIPO DE TRABAJO<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="normativa.php">NORMATIVA UNIDAD</a>
+				</li>
+				<a class="nav-link" href="http://172.16.142.79/osticket" target="_blank">MESA DE AYUDA ARTES</a>
+				</li>
+				<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+				data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				DESCARGAS
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="manuales.php">DESCARGA DE MANUALES</a>
+				<!--<a class="dropdown-item" href="#">TELÉFONOS</a>-->
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="instructivos.php">INSTRUCTIVOS</a>
+				</div>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="contacto.php">CONTACTO</a>
+				</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label
+				="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">BÚSQUEDA</button
+				>
+				</form>
+				</div>
+			</nav>
+		</div><br><br><br>
+
+	
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center;padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Eduroam" class="lateraliz" target="_blank">Señal Eduroam
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center; padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Servicio_VPN_UChile" class="lateraliz" target="_blank">VPN Corporativa
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center; padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Telefon%C3%ADa_IP" class="lateraliz">Telefonía IP
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-8 col-xl-8" style="text-align: justify;">
+					<h5>INSTRUCTIVOS</h5><br>
+					<div><img src="img/manual.jpg" alt="" style="float: right;width: 30%; align-self: center "></div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center;padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Licencias_de_Software" class="lateraliz" target="_blank">Licenciamiento
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center;padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Tarjeta_Universitaria_Inteligente" class="lateraliz" target="_blank">TUI
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+					<div class="lateralizq" style="height: 100px;">
+						<p style="text-align: center;padding-top: 13%">
+							<a href="https://soporte.uchile.cl/mediawiki/index.php/Correo_Corporativo" class="lateraliz" target="_blank">Correo Corporativo
+							</a> 
+						</p>
+			
+					</div><hr style="margin-left: 20px">
+				</div>
+			</div>
+
+		</div>
+
+		
+	
+	
+	<div class="container-fluid">
+		<footer class="footer">
+			<p style="text-align: center; color: #fff" class="my-auto">Derechos Reservados Unidad STC Facultad de Artes Universidad de Chile. 2019</p>
+			<p style="text-align: center; color: #fff" class="my-auto">Teléfonos de Contacto: 229787542 - 229780802</p>
+		</footer>
+	</div>
+	
+	<script src="js/lightbox-plus-jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.4.1.slim.min.js"></script>
+</body>
+
+</html>
